3018e8069a7eda4b15846975f763d7c1792725b1
Marcus Poulton <marcus.poulton@exify.net>
2025-06-25 18:01:30 +0100

    fix up gitignore

--- FarmScout/Services/INavigationService.cs
+++
@@ -1,8 +0,0 @@
-namespace FarmScout.Services;
-
-public interface INavigationService
-{
-    Task NavigateToAsync(string route);
-    Task NavigateToAsync(string route, IDictionary<string, object> parameters);
-    Task GoBackAsync();
-} 
