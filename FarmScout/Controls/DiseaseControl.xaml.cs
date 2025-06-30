f85c67400fd665f09b9fbfe7a4a44240f6f49ee3
extentsoftware <marcus.poulton@gmail.com>
2025-06-30 11:21:39 +0100

    button on disease

--- FarmScout/Controls/DiseaseControl.xaml.cs
+++ FarmScout/Controls/DiseaseControl.xaml.cs
@@ -1,10 +1,34 @@
+using FarmScout.Services;
+using FarmScout.Views;
+
 namespace FarmScout.Controls
 {
     public partial class DiseaseControl : ContentView
     {
         public DiseaseControl()
-        {
+        {            
             InitializeComponent();
+        }
+
+        private async void OnDiseaseInfoButtonClicked(object sender, EventArgs e)
+        {
+            var view = MauiProgram.Services.GetRequiredService<LookupPage>();
+            await Navigation.PushModalAsync(view);
+
+            var farmScoutDatabase = MauiProgram.Services.GetRequiredService<FarmScoutDatabase>();
+
+            var diseases = await farmScoutDatabase.GetLookupItemsByGroupAsync("Diseases");
+            var displayOptions = diseases.Select(x=>$"{x.Name} ({x.SubGroup}) - {x.Description}").ToArray();
+
+            var result = await Shell.Current.DisplayActionSheet(
+                "Select Disease",
+                "Cancel",
+                null,
+                displayOptions);
+
+            if (result != null && result != "Cancel")
+            {
+            }
         }
     }
 } 
