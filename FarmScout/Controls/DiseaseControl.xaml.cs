b4286120b99ae610109c24755be1660be0722b1f
Marcus Poulton <marcus.poulton@exify.net>
2025-07-02 07:30:35 +0100

    cleanup navigation

--- FarmScout/Controls/DiseaseControl.xaml.cs
+++ FarmScout/Controls/DiseaseControl.xaml.cs
@@ -13,22 +13,22 @@ namespace FarmScout.Controls
         private async void OnDiseaseInfoButtonClicked(object sender, EventArgs e)
         {
             var view = MauiProgram.Services.GetRequiredService<LookupPage>();
-            await Shell.Current. Navigation.PushModalAsync(view);
+            await Shell.Current.Navigation.PushModalAsync(view);
 
-            var farmScoutDatabase = MauiProgram.Services.GetRequiredService<FarmScoutDatabase>();
+            //var farmScoutDatabase = MauiProgram.Services.GetRequiredService<FarmScoutDatabase>();
 
-            var diseases = await farmScoutDatabase.GetLookupItemsByGroupAsync("Diseases");
-            var displayOptions = diseases.Select(x=>$"{x.Name} ({x.SubGroup}) - {x.Description}").ToArray();
+            //var diseases = await farmScoutDatabase.GetLookupItemsByGroupAsync("Diseases");
+            //var displayOptions = diseases.Select(x=>$"{x.Name} ({x.SubGroup}) - {x.Description}").ToArray();
 
-            var result = await Shell.Current.DisplayActionSheet(
-                "Select Disease",
-                "Cancel",
-                null,
-                displayOptions);
+            //var result = await Shell.Current.DisplayActionSheet(
+            //    "Select Disease",
+            //    "Cancel",
+            //    null,
+            //    displayOptions);
 
-            if (result != null && result != "Cancel")
-            {
-            }
+            //if (result != null && result != "Cancel")
+            //{
+            //}
         }
     }
 } 
